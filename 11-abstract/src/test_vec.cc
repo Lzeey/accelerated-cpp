bb790ab696ec52bbcfadc2acd1f3016b0137326b
lzeey <zeyi@berkeley.com>
2018-10-22 17:25:10 +0800

    Chapter 11 - Vec implementation

---
+++ 11-abstract/src/test_vec.cc
@@ -0,0 +1,23 @@
+/* Unit testing for Vec class */
+#include <iostream>
+#include <string>
+
+#include "Vec.h"
+
+using std::cout; using std::endl;
+
+int test_init() {
+    Vec<int> v; //Default constructor
+    Vec<double> w(10); //Explicit
+    Vec<std::string> strings(10, "Hello"); //Explicit
+    strings.push_back("BBB");
+    for (Vec<std::string>::const_iterator it = strings.begin(); it != strings.end(); ++it) {
+        cout << *it << '\n';
+    }
+    return 0;
+}
+
+int main() {
+    test_init();
+    return 0;
+}
